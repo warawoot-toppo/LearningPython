7874bc138ad9478186e0a208804a9d9985097b7c
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-18 14:57:23 +0700

    add Reserved_Word

---
+++ variable/Identifier/Reserved_Word.py
@@ -0,0 +1,14 @@
+from re import S
+
+
+# คำที่ห้ามใช้ตั้งชื่อตัวแปร เช่น
+
+if = S
+
+for = 5
+
+del = 5
+
+# คำที่ห้ามใช้ตั้งชื่อตัวแปรมีดังนี้  
+# and, assert, break, class, continue, def, del, elif, else, except, exec, finally, for, from, global, if, import,
+# in, is, lambda, not, or, pass, print, raise, return, try, while, yield
