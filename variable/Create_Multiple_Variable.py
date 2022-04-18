34afdff61365f3d4b26a788a155467904a6a3620
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-18 14:11:20 +0700

    add Create_Multiple_Variable

---
+++ variable/Create_Multiple_Variable.py
@@ -0,0 +1,17 @@
+c = d = e = 5
+
+print('c =', c)
+print('d =', d)
+print('e =', e) + '\n'
+
+f = 6; g = 7; h = 8;
+
+print('f =', f)
+print('g =', g)
+print('h =', h)
+
+q, j, k = 1, 2, 3
+
+print('q =', q)
+print('j =', j)
+print('k =', k)
