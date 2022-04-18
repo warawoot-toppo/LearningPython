f23c336666737c62937ba471e41abf6222bca2bc
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-18 13:58:49 +0700

    add Create_and_Assign

---
+++ variable/Create_and_Assign.py
@@ -0,0 +1,16 @@
+firstname = 'John'
+lastname = 'Doe'
+age_full = 'I\'m 32 years old.'
+
+print('My firstname is', firstname + '.')
+print('My lastname is', lastname + '.')
+print(age_full)
+
+
+
+a = 5
+b = 7
+
+print('a =', a)
+print('b =', b)
+print('a + b =', a + b)
