47ea93579113de49ad495850679fe0ec6281a077
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-19 16:19:12 +0700

    add folder

---
+++ Data_Structure/Composite_Data_Structure/List/Update_List/Update_List_append.py
@@ -0,0 +1,9 @@
+list7 = [1, 2, 3, 'a', 'b', 'c']
+
+list7.append('d')
+
+print(list7)
+
+list7.append('e')
+
+print(list7)
---
+++ Data_Structure/Composite_Data_Structure/List/Update_List/Update_List_extend.py
@@ -0,0 +1,6 @@
+list8 = [1, 2, 3]
+list9 = ['a', 'b', 'c']
+
+list8.extend(list9)
+
+print(list8)
