b07d4c756d8135013e81d3bcba3caa8df0af2503
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-19 16:41:05 +0700

    add List+sort

---
+++ Data_Structure/Composite_Data_Structure/List/List+sort.py
@@ -0,0 +1,17 @@
+list14 = [1, 5, 4, 2, 3]
+
+list14.sort()
+
+print(list14)
+
+list14.sort(reverse = True)
+
+print(list14)
+
+Sorted_list14 = sorted(list14)
+
+print(Sorted_list14)
+
+Sorted_list14_1 = sorted(list14, reverse = True)
+
+print(Sorted_list14_1)
