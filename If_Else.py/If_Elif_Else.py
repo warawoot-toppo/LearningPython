9c53f6fdd216b0a96a667eb96d0057dc133c5c37
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-21 19:47:40 +0700

    add If_Elif_Else.py

---
+++ If_Else.py/If_Elif_Else.py
@@ -0,0 +1,8 @@
+name = input('Please insert your name: ')
+if name == 'John':
+    print('Hi John')
+elif name == 'Alan':
+    print('Hi', name)
+else:
+    print('ใครวะ ?')
+print('ขอตังค์หน่อยจิ')    
