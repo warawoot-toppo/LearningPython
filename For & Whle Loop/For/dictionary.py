56152c67cdf74bed26f0b18964160aebc494202c
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-23 04:49:50 +0700

    add dictionary.py

---
+++ For & Whle Loop/For/dictionary.py
@@ -0,0 +1,13 @@
+dictx = {'firstname' : 'John', 'lastname' : 'Doe', 'age' : 32}
+for key in dictx:
+    print(key)
+
+print('-------------------------')
+
+for key in dictx.keys():
+    print(key)
+
+print('-------------------------')
+
+for value in dictx.values():
+    print(value)
