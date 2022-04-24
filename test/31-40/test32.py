5362a129fd4474e96cb223399f844c54fe19afd7
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-25 05:40:00 +0700

    add test32.py

---
+++ test/31-40/test32.py
@@ -0,0 +1,7 @@
+'''
+เขียนโปรแกรมรับอินพุต 1 ตัวที่เป็น str จากนั้นให้พิมพ์ str ที่ index เป็นเลขคู่
+'''
+str1 = input('str1: ')
+for i in range(len(str1)):
+    if i % 2 == 0:
+        print(str1[i])
