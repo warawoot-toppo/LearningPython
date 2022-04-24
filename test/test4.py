729f3c8ec17a86d0cda9b01ee8670c1615fc3471
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-24 15:25:46 +0700

    add test4.py

---
+++ test/test4.py
@@ -0,0 +1,8 @@
+'''
+เขียนโปรแกรมสร้าง empty list และ append ค่า 0, 1, 2,..., 19 ลงไปใน list นั้น
+หลังจากนั้นให้พิมพ์ค่า list ที่สร้างออกมา
+'''
+list1 = []
+for i in range(20):
+    list1.append(i)
+print(list1)    
