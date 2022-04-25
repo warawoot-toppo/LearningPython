2d402b76940a3663fbf8de6f49f800cfef807139
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-25 07:28:33 +0700

    add test40.py

---
+++ test/31-40/test40.py
@@ -0,0 +1,8 @@
+'''
+เขียนโปรแกรมรับ input 5 ตัวที่เป็น str โดยใช้ for loop จากนั้นให้ทำการต่อ str ทั้งหมดที่รับและพิมพ์ออกมา
+'''
+str2 = ''
+for i in range(5):
+    str1 = input('str1: ')
+    str2 = str2 + str1
+print(str2)    
