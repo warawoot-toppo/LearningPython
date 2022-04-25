b833f0fe67273cb2ccb8f47712dae0ede7e0fe7d
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-25 22:58:58 +0700

    add test59.py

---
+++ test/51-60/test59.py
@@ -0,0 +1,15 @@
+'''
+กำหนดให้ matrix1 = [[1, 2, 3, 4], [5, 6, 7, 8]] เขียนโปรแกรม transpose matrix1 และพิมพ์ผลลัพธ์ออกมา
+'''
+matrix1 = [[1, 2, 3, 4], 
+           [5, 6, 7, 8]]
+tran_matrix = [[0, 0], 
+               [0, 0], 
+               [0, 0], 
+               [0, 0]]
+n_rows = len(matrix1)
+n_cols = len(matrix1[0])
+for r in range(n_rows):
+    for c in range(n_cols):
+        tran_matrix[c][r] = matrix1[r][c]
+print(tran_matrix)
