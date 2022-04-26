7dcd357e88ac1e67265f3116556c804cc1352440
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-26 15:40:50 +0700

    add 63.py

---
+++ test/61-70/63.py
@@ -0,0 +1,13 @@
+'''
+กำหนดให้ tuple2 = (1, 2, 3,...,25) เขียนโปรแกรมหาจำนวนเฉพาะใน tuple2 และพิมพ์ออกมา
+'''
+tuple2 = tuple(x for x in range(1, 26))
+
+count = 0
+for i in tuple2:
+    count = 0
+    for j in range(1, i + 1):
+        if i % j == 0:
+            count = count + 1
+    if count == 2: 
+        print(i)
