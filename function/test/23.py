c5823da5f60af76854a6c3c36cb71b024c390c57
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-29 20:00:25 +0700

    add test

--- function/test/23.py
+++ function/test/23.py
@@ -1,13 +1,13 @@
 def coin_changer(amount):
-    coin_list = ['10', '5', '2', '1']
-    coin_dict = {'10' : 0, '5' : 0, '2' : 0, '1' : 0}
+    coin_list = ['1000', '500', '100', '50', '10', '5', '2', '1']
+    coin_dict = {'1000' : 0, '500' : 0, '100' : 0, '50' : 0, '10' : 0, '5' : 0, '2' : 0, '1' : 0}
     for c in coin_list:
         if amount == 0:
             break
         else:
             n_coin = amount // int(c)
             coin_dict[c] = n_coin
-            amount = amount - n_coin * int(c)
+            amount -= n_coin * int(c)
     return coin_dict
 
-print(coin_changer(58))            
+print(coin_changer(21535))            
