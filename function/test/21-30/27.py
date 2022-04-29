4ae349f1fb9a8b95ab7fd8828f8b6a08a4746c3b
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-30 03:30:53 +0700

    test

---
+++ function/test/21-30/27.py
@@ -0,0 +1,12 @@
+def calculator(a,b,op):
+
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    elif op == '/':
+        return a / b    
+
+print(calculator(2,2,'*'))        
