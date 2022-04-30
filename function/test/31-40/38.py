6edf67c9216d3a5f8abd3d0028e64a41ab7fa080
warawoot-toppo <warawoot.sur@gmail.com>
2022-04-30 17:11:57 +0700

    test

---
+++ function/test/31-40/38.py
@@ -0,0 +1,12 @@
+def isprime(num):
+    count = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
+    if count == 2:
+        return True
+    else:
+        return False
+
+
+print(isprime(5))
