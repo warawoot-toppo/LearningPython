f8a6645babdb1623993728174c685bc155136077
warawoot-toppo <warawoot.sur@gmail.com>
2022-05-02 02:27:26 +0700

    add 10.py

--- OOP/test/10.py
+++ OOP/test/10.py
@@ -3,7 +3,7 @@ class Cashier:
         self.products = products
     def recommend(self):
         self.products_list = ', '.join(self.products)
-        print('We have ' + self.products_list + '.')
+        print('We have ' + str(self.products_list) + '.')
 
 cashier = Cashier(['apple', 'banana', 'orange'])
 cashier.recommend()
