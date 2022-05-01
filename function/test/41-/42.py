d70c9b9ac46fbe129b1114203b1b19385523668b
warawoot-toppo <warawoot.sur@gmail.com>
2022-05-01 16:00:56 +0700

    test

---
+++ function/test/41-/42.py
@@ -0,0 +1,10 @@
+def draw_tree3():
+    for i in range(4):
+        for j in range(4 + i):
+            if j < 3 - i:
+                print('-', end = '')
+            else:
+                print('x', end = '')
+        print()
+
+draw_tree3()                     
