0d0d1f4b28fa02e7d85a45e25e6f37ae315df885
warawoot-toppo <warawoot.sur@gmail.com>
2022-05-01 17:55:17 +0700

    attribute 2

---
+++ OOP/attribute/2.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, brand, model, color):
+        self.brand = brand
+        self.model = model
+        self.color = color
+car1 = Car('Toyota', 'Camry', 'Black')
+print(car1.brand)
+print(car1.model)
+print(car1.color)        
